6b143f980da74780d95b39fe870acaf4097a6209
nikakowalska <142528082+nikakowalska@users.noreply.github.com>
2024-02-13 11:27:47 +0100

    controller

--- src/main/java/pl/dicedev/game/controllers/UserTokenController.java
+++ src/main/java/pl/dicedev/game/controllers/UserTokenController.java
@@ -4,14 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.dicedev.game.clients.AuthClient;
 
+import static pl.dicedev.game.controllers.URINames.ENDPOINT_LOGIN;
+import static pl.dicedev.game.controllers.URINames.ENDPOINT_USER;
+
 @RestController
+@RequestMapping(ENDPOINT_LOGIN)
 public class UserTokenController {
     @Autowired
     private AuthClient authClient;
 
-    @GetMapping("/credentials")
-    public String getToken() {
-        return authClient.getToken();
+
+    @PostMapping("validate/{token}")
+    public boolean getValidateToken(@PathVariable String token) {
+        String retrievedToken = authClient.getToken();
+        return retrievedToken.equals(token);
     }
 }
 
