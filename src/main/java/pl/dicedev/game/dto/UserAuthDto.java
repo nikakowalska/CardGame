8c7c216b3c7f8a5603e52fe5687d835c4423dec3
nikakowalska <142528082+nikakowalska@users.noreply.github.com>
2024-02-13 18:33:12 +0100

    controller

---
+++ src/main/java/pl/dicedev/game/dto/UserAuthDto.java
@@ -0,0 +1,58 @@
+package pl.dicedev.game.dto;
+
+import java.util.Objects;
+
+public class UserAuthDto {
+    private String username;
+    private String password;
+
+    public UserAuthDto() {
+    }
+
+    public UserAuthDto(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final UserAuthDto that = (UserAuthDto) o;
+
+        if (!Objects.equals(username, that.username)) return false;
+        return Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAuthDto{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
